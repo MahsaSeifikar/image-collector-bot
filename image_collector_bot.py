64a1d06495f2429a526d883c08bb7d4f34184ef3
Mahsa Seifikar <m.seifikar@sdb247.com>
2020-06-21 17:58:02 +0430

    Handle two uploaded photo.

--- image_collector_bot.py
+++ image_collector_bot.py
@@ -2,32 +2,63 @@ from telebot import TeleBot, types
 from simple_settings import settings
 
 bot = TeleBot(settings.ACCESS_TOKEN)
-markup = types.ReplyKeyboardMarkup(one_time_keyboard=True, row_width=1)
-markup.add("Read-Wiki", "Skip")
+
+markup = types.ReplyKeyboardMarkup(row_width=2)
+itembtn1 = types.KeyboardButton('شروع')
+itembtn2 = types.KeyboardButton('خروج')
+itembtn3 = types.KeyboardButton('عکس کارت ملی')
+itembtn4 = types.KeyboardButton('عکس سلفی')
+markup.add(itembtn2, itembtn1,
+           itembtn4, itembtn3)
+
+choose = None
+
+
+def choose_button(message):
+    bot.send_message(message.chat.id, settings.CHOOSE_BUTTON_MESSAGE, reply_markup=markup)
 
 
 # Handle '/start' and '/help'
 @bot.message_handler(commands=['help', 'start'])
 def send_welcome(message):
     bot.reply_to(message, settings.WELCOME_MESSAGE)
+    choose_button(message)
 
 
 # Handle photo
 @bot.message_handler(content_types=['photo'])
 def save_photo(message):
-    fileID = message.photo[-1].file_id
-    file_info = bot.get_file(fileID)
-    downloaded_file = bot.download_file(file_info.file_path)
-    with open("test%d.jpg" % message.message_id, 'wb') as new_file:
-        new_file.write(downloaded_file)
+    global choose
+    if choose:
+        fileID = message.photo[-1].file_id
+        file_info = bot.get_file(fileID)
+        downloaded_file = bot.download_file(file_info.file_path)
+        image_name = '%s-%d.jpg' % (choose, message.from_user.id)
+        with open(image_name, 'wb') as new_file:
+            new_file.write(downloaded_file)
+        choose = None
+        bot.send_message(message.chat.id, settings.CHOOSE_BUTTON_MESSAGE, reply_markup=markup)
+    else:
+        bot.send_message(message.chat.id, settings.CHOOSE_PHOTO_MESSAGE, reply_markup=markup)
 
 
 # Handle all other messages with content_type 'text' (content_types defaults to ['text'])
 @bot.message_handler(func=lambda message: True)
 def echo_message(message):
+    global choose
     print(message)
-    bot.reply_to(message, message.text)
+    if message.text == 'عکس سلفی':
+        choose = 'selfie'
+        bot.reply_to(message, settings.SELFIE_PHOTO_MESSAGE)
+
+    if message.text == 'عکس کارت ملی':
+        choose = 'national'
+        bot.reply_to(message, settings.NATIONAL_CARD_PHOTO_MESSAGE)
+    if message.text == 'شروع':
+        send_welcome(message)
+
+    if message.text == 'خروج':
+        bot.reply_to(message, 'با تشکر از همکاری شما.')
 
 
-counter = 1
 bot.polling()
